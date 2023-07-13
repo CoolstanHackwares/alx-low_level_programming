5e4f0a39754c442a20e8736848748a493e816193
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-07-13 16:36:35 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - initial value entry point
+*return : always 0 SUCCESS
+**/
+int main(void)
+{
+/*Program Initialization*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
