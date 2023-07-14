2b622d737a17b85cdfaac3229f5737dd8e9bf70d
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-07-13 17:53:10 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A program that prints text to the screen using printf
+* Return: 0 (SUCCESSFUL)
+**/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
