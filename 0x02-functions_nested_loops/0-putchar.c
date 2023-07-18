648fb5c832577bb38f2c92c61616cc4eb7ad1a4b
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-07-18 02:07:54 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry Point
+ * Description: This program prints _putchar to the screen
+ * followed by a new line
+ * without using the printf and puts function.
+ * This program conforms to the betty documentation style.
+ * Return: 0 (SUCCESSFUL)
+ **/
+
+int main(void)
+{
+	char message[] = "_putchar\n";
+
+	for (int a = 0; message[a] != '\0'; a++)
+	{
+	char c = message[a];
+
+	write(1, &c, 1);
+	}
+	return (0);
+}
