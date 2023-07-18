cfd6b81334133c138d1bba8af97b111f2fac7971
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-07-18 16:19:46 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _islower - This function checks for a lowercase character
- * @e: The character to check
+ * @c: The character to check
  * Return: 1 if c is lowercase, 0 otherwise.
  * This function conforms to the betty documentation style
  **/
 
-int _islower(int e)
+int _islower(int c)
 {
-	return (e >= 'a' && e <= 'z');
+	return (c >= 'a' && c <= 'z');
 }
