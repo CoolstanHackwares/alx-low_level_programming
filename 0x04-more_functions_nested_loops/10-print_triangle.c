3174846a1b1ca0e5cd9c763198a81eb6483bb462
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-07-20 22:03:21 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,9 +17,13 @@ void print_triangle(int size)
 {
 	int a, b;
 
-	for (a = 1; a <= size; a++)
+	for (a = 0; a <= size; a++)
 {
-	for (b = 1; b <= a; b++)
+	for (b = 0; b <= a; b++)
+{
+	_putchar(' ');
+}
+	for (b = 0; b <= a; b++)
 {
 	_putchar('#');
 }
