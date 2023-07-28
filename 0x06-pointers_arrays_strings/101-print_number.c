7ed293c1fe8213354e70704750ba53c63c863cd4
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-07-27 18:27:50 -0700

    Numbers have life; they're not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - A function that Prints an integer
+ * @n: The integer to be printed
+ * This function prints the integer 'n' using only the _putchar function.
+ * This program conforms to the betty documentation style
+ **/
+
+void print_number(int n)
+{
+	if (n < 0)
+{
+	_putchar('-');
+	n = -n;
+}
+
+	if (n / 10 != 0)
+{
+	print_number(n / 10);
+}
+
+	_putchar('0' + (n % 10));
+}
+
