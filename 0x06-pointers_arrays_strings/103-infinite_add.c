29664cb2ae41e3ae1c8c8aefa94ade9341e0b34b
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-07-28 17:24:34 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,41 +12,37 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
-	int len1 = 0, len2 = 0;
 
-	while (n1[len1])
-	len1++;
-	while (n2[len2])
-	len2++;
-
-	if (len1 >= size_r - 1 || len2 >= size_r - 1)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+	;
+	if (i > size_r || j > size_r)
 	return (0);
-
-	i = len1 - 1;
-	j = len2 - 1;
-	k = 0;
 	m = 0;
-
-	while (i >= 0 || j >= 0 || m)
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 {
 	n = m;
 	if (i >= 0)
-	n += n1[i--] - '0';
+	n += n1[i] - '0';
 	if (j >= 0)
-	n += n2[j--] - '0';
-
-	m = n / 10;
-	r[k++] = n % 10 + '0';
+	n += n2[j] - '0';
+	if (i < 0 && j < 0 && n == 0)
+{
+	break;
 }
-	for (l = 0, k -= 1; l < k; k--, l++)
+	m = n / 10;
+	r[k] = n % 10 + '0';
+}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+	return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
 {
 	m = r[k];
 	r[k] = r[l];
 	r[l] = m;
 }
-
-	r[k] = '\0';
-
 	return (r);
 }
 
