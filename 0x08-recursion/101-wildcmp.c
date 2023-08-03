6f61ca4a989e63866088b81d17a6588ac9f1878f
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-02 20:45:28 -0700

    Inception. Now, before you bother telling me it's impossible...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * wildcmp - A function that compares two strings
+ * @s1: The first string
+ * @s2: The second string
+ * Return: 1 if the strings can be considered identical,
+ * otherwise return 0
+ * This program conforms to the betty documentation style
+ **/
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+{
+	return (1);
+}
+	else if (*s1 == *s2 || *s2 == '*')
+{
+	return (wildcmp(s1 + 1, s2 + 1));
+}
+	else if (*s2 == '\0')
+{
+	return (0);
+}
+	else if (*s1 == '\0' && *s2 != '*')
+{
+	return (0);
+}
+	else if (*s2 == '*')
+{
+	return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+}
+	else
+{
+	return (0);
+}
+}
