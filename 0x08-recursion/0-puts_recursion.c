f359ad9413528a86f528013afd60fd3481267976
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-02 17:21:06 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,6 +4,7 @@
  * _puts_recursion - A function that prints a string followed by a new line
  * using recursion.
  * @s: The string to print
+ * Return: Nothing
  * This program conforms to the betty documentation style.
  **/
 
@@ -11,7 +12,9 @@ void _puts_recursion(char *s)
 
 {
 	if (*s == '\0')
-{	_putchar('\n');
+{
+	_putchar('\n');
+	return;
 }
 	_putchar(*s);
 	_puts_recursion(s + 1);
