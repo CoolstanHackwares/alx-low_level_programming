4d81e63a3170c7791a0c9054f0f392c5f5b7a688
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-07 15:42:20 -0700

    It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - A function that frees a 2 dimensional grid
+ * previously created by 3-alloc_grid.c
+ * @grid: The address of the two dimensional grid
+ * @height: The height of the grid
+ * This program conforms to the betty documentation style
+ **/
+
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	if (grid == NULL || height <= 0)
+
+	for (a = 0; a < height; a++)
+{
+	free(grid[a]);
+}
+
+	free(grid);
+}
