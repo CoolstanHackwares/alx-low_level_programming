41166951ca079a8d4420c0b2f98ce9e0ab069579
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-08 16:48:37 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,88 +1,133 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
+
+/**
+ * is_space - A helper function that checks words for spaces
+ * @c: The string to check
+ * Return: Nothing
+ * This program conforms to the betty documentation style
+ **/
+
+int is_space(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n');
+}
+
+/**
+ * count_words - A helper function that counts the number of words in a string
+ * @str: The string to check
+ * Return: Nothing
+ * This program conforms to the betty documentation style
+ **/
+
+int count_words(char *str)
+{
+	int word_count;
+	int in_word;
+
+	word_count = 0;
+	in_word = 0;
+	while (*str)
+{
+	if (!is_space(*str))
+{
+	if (!in_word)
+{
+	in_word = 1;
+	word_count++;
+}
+}
+	else
+{
+	in_word = 0;
+}
+	str++;
+}
+
+	return (word_count);
+}
+
+/**
+ * strdup_custom - A helper function that duplicates a string
+ * @str: The string to duplicate
+ * @len: The length of the string
+ * Return: Nothing
+ **/
+
+char *strdup_custom(char *str, int len)
+{
+	int i;
+char *new_str = (char *)malloc((len + 1) * sizeof(char));
+	if (new_str)
+{
+	for (i = 0; i < len; i++)
+{
+	new_str[i] = str[i];
+}
+	new_str[len] = '\0';
+}
+	return (new_str);
+}
 
 /**
  * strtow - A function that splits a string into words
- * @str: The input string to split
- * Return: Pointer to an array of strings (words), or NULL on failure
+ * @str: The string to check
+ * Return: NULL if str == NULL or str == "" or NULL if it fails
  * This program conforms to the betty documentation style
  **/
 
 char **strtow(char *str)
 {
-	int words_count = 0;
-	int in_word = 0;
-	char **words = NULL;
-	char *word_start = NULL;
+	int word_count;
+	char **words;
+	int index;
+	int in_word;
+	int word_len;
 
 	if (str == NULL || *str == '\0')
 {
 	return (NULL);
 }
-	for (char *c = str; *c != '\0'; c++)
-{
-	if (*c == ' ' || *c == '\t' || *c == '\n')
-{
-	if (in_word)
-{
-	in_word = 0;
-	words_count++;
-}
-}
-	else
-{
-	if (!in_word)
-{
-	in_word = 1;
-	word_start = c;
-}
-}
-}
-	if (in_word)
-{
-	words_count++;
-}
-	if (words_count == 0)
-{
-	return (NULL);
-}
-words = (char **)malloc((words_count + 1) * sizeof(char *));
+
+	word_count = count_words(str);
+	words = (char **)malloc((word_count + 1) * sizeof(char *));
 	if (words == NULL)
 {
 	return (NULL);
 }
 
+	index = 0;
 	in_word = 0;
-	words_count = 0;
-	for (char *c = str; *c != '\0'; c++)
+	word_len = 0;
+
+	while (*str)
 {
-	if (*c == ' ' || *c == '\t' || *c == '\n')
-{
-	if (in_word)
-{
-	in_word = 0;
-	*c = '\0';
-	words[words_count] = word_start;
-	words_count++;
-}
-}
-	else
+	if (!is_space(*str))
 {
 	if (!in_word)
 {
 	in_word = 1;
-	word_start = c;
+	word_len = 0;
+}
+	word_len++;
+}
+	else
+{
+	if (in_word)
+{
+	words[index++] = strdup_custom(str - word_len, word_len);
+	in_word = 0;
 }
 }
+	str++;
 }
 
 	if (in_word)
 {
-	words[words_count] = word_start;
-	words_count++;
+	words[index++] = strdup_custom(str - word_len, word_len);
 }
-	words[words_count] = NULL;
+
+	words[index] = NULL;
 
 	return (words);
 }
-
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 
 int _putchar(char c);
+int is_space(char c);
+int count_words(char *str);
+char *strdup_custom(char *str, int len);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
