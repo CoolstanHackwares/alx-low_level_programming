b551e13613ceb8bc2e167821e6052e15d65739d2
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-14 06:15:21 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A program that displays basic information about a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ * Description: struct called "dog" that stores information about a dog
+ * and its owner.
+ **/
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
+
