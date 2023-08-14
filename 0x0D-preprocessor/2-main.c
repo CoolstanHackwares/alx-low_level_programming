18e97a776d9665cc91929dcef8ae9f13c8479637
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-14 04:04:58 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the name of the file it was compiled from,
+ * followed by a new line.
+ *
+ * Return: 0 (Successful)
+ * This program conforms to the betty documentation style
+ **/
+
+int main(void)
+{
+	Printf("%s/n", __FILE__);
+	return (0);
+}
