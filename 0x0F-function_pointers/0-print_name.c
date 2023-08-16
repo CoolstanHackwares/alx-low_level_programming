62b374a9ba54033da4ea19a63d561fe9add41de2
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-16 15:20:44 -0700

    What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,15 +4,14 @@
 /**
  * print_name - A function that prints a name
  * @name: The name to print
- * @f: a pointer to a function
+ * @f: a pointer to a function that takes a char as argument
+ * and returns void.
  * This program conforms to the betty documentation style
  **/
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-{
 	f(name);
-}
 }
 
