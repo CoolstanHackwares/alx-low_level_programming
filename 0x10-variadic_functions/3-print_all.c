76a09a8e7f1bfe5e01924dc3aa64b08a7645da60
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-17 18:23:58 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,108 +1,99 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_char - A function that prints a char argument.
- * @args: The va_list containing the argument.
- * This program conforms to the betty documentation style
- **/
+  * print_char -  A function that prints a character
+  * @separator: The character separator
+  * @args: The list of arguments passed to the program
+  * Return: Nothing
+  * This program conforms to the betty documentation style
+  **/
 
-void print_char(va_list args)
+void print_char(char *separator, va_list args)
 {
-	char c;
-
-	c = va_arg(args, int);
-	printf("%c", c);
+	printf("%s%c", separator, va_arg(args, int));
 }
 
 /**
- * print_int - A function that prints an int argument.
- * @args: The va_list containing the argument.
- * This program conforms to the betty documentation style
- **/
+  * print_int - A function that  prints an integer type character
+  * @separator: The character separator
+  * @args: The list of arguments passed to the program
+  * Return: Nothing
+  * This program conforms to the betty documentation style
+  **/
 
-void print_int(va_list args)
+void print_int(char *separator, va_list args)
 {
-	int i;
-
-	i = va_arg(args, int);
-	printf("%d", i);
+	printf("%s%i", separator, va_arg(args, int));
 }
 
 /**
- * print_float - A function that prints a float argument.
- * @args: The va_list containing the argument.
- * This program conforms to the betty documentation style
- **/
+  * print_float - PA function that prints a float type character
+  * @separator: The character separator
+  * @args: The list of arguments passed to the prrogram
+  * Return: Nothing
+  * This program conforms to the betty documentation style
+  **/
 
-void print_float(va_list args)
+void print_float(char *separator, va_list args)
 {
-	double f;
-
-	f = va_arg(args, double);
-	printf("%f", f);
+	printf("%s%f", separator, va_arg(args, double));
 }
 
 /**
- * print_string - A function that prints a string argument.
- * @args: The va_list containing the argument.
- * This program conforms to the betty documentation style
- **/
-
-void print_string(va_list args)
+  * print_char_ptr - Prints the content of pointer to char type
+  * @separator: The separator of the character
+  * @args: A list of variadic arguments
+  * Return: Nothing
+  */
+void print_char_ptr(char *separator, va_list args)
 {
-	char *s;
+	char *arg = va_arg(args, char *);
 
-	s = va_arg(args, char *);
-	if (s == NULL)
-	printf("(nil)");
-	else
-	printf("%s", s);
+	if (arg == NULL)
+{
+	printf("%s%s", separator, "(nil)");
+
+}
+	printf("%s%s", separator, arg);
 }
 
 /**
- * print_all - A function that print anything based on a format string.
- * @format: The format string containing argument types.
- * c: char
- * i: integer
- * f: float
- * s: string (if NULL, print "(nil)")
- * Any other character is ignored.
- * ...: The arguments to be printed.
- * This program conforms to the betty documentation style
- **/
+  * print_all - A function that prints anything according to a specified format
+  * @format: The list of argument types passed to the function
+  * This program conforms to the betty documentation style
+  **/
 
 void print_all(const char * const format, ...)
 {
-	void (*print_functions[4])(va_list);
-	const char *format_ptr = format;
 	va_list args;
-
-	print_functions[0] = print_char;
-	print_functions[1] = print_int;
-	print_functions[2] = print_float;
-	print_functions[3] = print_string;
+	p_dt form_types[] = {
+		{ "c", print_char },
+		{ "i", print_int },
+		{ "f", print_float },
+		{ "s", print_char_ptr }
+	};
+	unsigned int a = 0;
+	unsigned int b = 0;
+	char *separator = "";
 
 	va_start(args, format);
-
-	while (*format_ptr)
+	while (format != NULL && format[a])
 {
-	if (*format_ptr == 'c')
-	print_functions[0](args);
-	else if (*format_ptr == 'i')
-	print_functions[1](args);
-	else if (*format_ptr == 'f')
-	print_functions[2](args);
-	else if (*format_ptr == 's')
-	print_functions[3](args);
-
-	format_ptr++;
-	if (*format_ptr)
-	printf(", ");
+	b = 0;
+	while (b < 4)
+{
+	if (format[a] == *form_types[b].identifier)
+{
+	form_types[b].p(separator, args);
+	separator = ", ";
 }
-
+	b++;
+}
+	a++;
+}
 	va_end(args);
-
 	printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,9 +8,21 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list args);
-void print_int(va_list args);
-void print_float(va_list args);
-void print_string(va_list args);
+void print_char(char *separator, va_list args);
+void print_int(char *separator, va_list args);
+void print_float(char *separator, va_list args);
+void print_char_ptr(char *separator, va_list args);
+
+/**
+ * struct format_types - The format_types of the struct
+ * @identifier: The specifier used in the conversion
+ * @p: The pointer to the function being called
+ **/
+
+typedef struct format_types
+{
+	char *identifier;
+	void (*p)(char *separator, va_list args);
+} p_dt;
 
 #endif
