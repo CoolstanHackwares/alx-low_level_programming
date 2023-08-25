8b8a2b525738b93dd05dd54376574706ed10686d
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-24 18:22:34 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,9 +17,12 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
+	if (h == NULL)
+		return (0);
+
 	while (h)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
