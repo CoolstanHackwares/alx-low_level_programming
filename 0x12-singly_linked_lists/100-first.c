a843ad3b48009b87faf42694fad9fc16c7dea31c
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-24 19:59:18 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,7 +10,7 @@
 
 void __attribute__((constructor)) message(void)
 {
-	printf("You're beat! and yet, you must allow,\n")
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
 
