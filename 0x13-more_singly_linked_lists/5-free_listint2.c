6d5af80f389d65423be5946610a9bf4de2dabcd4
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-28 11:51:57 -0700

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - A function that frees a listint_t list
+ * and sets the head to NULL.
+ * @head: A pointer to a pointer to the head of the linked list.
+ * This code conforms to the betty documentation style
+ **/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+{
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+}
+	*head = NULL;
+}
+
