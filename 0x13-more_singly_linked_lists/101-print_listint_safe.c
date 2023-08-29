d4e841a0834127876400b69712e0ed85ba80f3bb
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-08-29 11:39:28 -0700

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,40 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * is_in_loop - A helper function that checks if a given node is present
- * in the linked list.
- * @head: A pointer to the head of the linked list.
- * @node: A pointer to the node to be checked.
- * Return: 1 if the node is in the list, 0 otherwise.
- * This program conforms to the betty documentation style.
- **/
-
-int is_in_loop(const listint_t *node)
-{
-	const listint_t *tortoise;
-	const listint_t *hare;
-
-	tortoise = node;
-	hare = node;
-
-	while (hare != NULL && hare->next != NULL)
-{
-	tortoise = tortoise->next;
-	hare = hare->next->next;
-
-	if (tortoise == hare)
-{
-	return (1);
-}
-}
-
-	return (0);
-}
-
-/**
- * print_listint_safe - A function that prints a listint_t linked list.
+ * print_listint_safe - A function that prints a listint_t linked list
+ * in reverse order.
  * @head: A pointer to the head of the linked list.
  * Return: The number of nodes in the list.
  * This program conforms to the betty documentation style.
@@ -43,20 +13,20 @@ int is_in_loop(const listint_t *node)
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *current;
+	const listint_t *current = head;
 
-	current = head;
-
-	while (current != NULL && !is_in_loop(current))
+	while (current)
 {
 	printf("[%p] %d\n", (void *)current, current->n);
 	count++;
-	current = current->next;
+
+	if (current <= current->next)
+{
+	printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+	break;
 }
 
-	if (current != NULL)
-{
-	printf("[%p] %d\n", (void *)current, current->n);
+	current = current->next;
 }
 
 	return (count);
