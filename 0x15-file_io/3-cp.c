10a29c1dd1444f76ee55faf0d59f625cc42d7a59
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2023-09-04 19:22:33 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,17 +1,4 @@
 #include "main.h"
-#define BUFF_SIZE 1024
-
-/**
- * error_exit - A helper function to print an error message and exit
- * @message: The error message to print
- * @exit_code: The exit code to use when terminating the program
- * This program conforms to the betty documentation style
- **/
-void error_exit(const char *message, int exit_code)
-{
-	dprintf(STDERR_FILENO, "Error: %s\n", message);
-	exit(exit_code);
-}
 
 /**
  * main - A function that copies the content of a file to another file
@@ -20,46 +7,47 @@ void error_exit(const char *message, int exit_code)
  * Return: 0 (Successful)
  * This program conforms to the betty documentation style
  **/
+
 int main(int argc, char *argv[])
 {
-	int fd_f, fd_t;
-	ssize_t br, bw;
-	char buffer[BUFF_SIZE];
+	int fd_f, fd_t, x, a, b;
+	char buffer[BUFSIZ];
 
 	if (argc != 3)
-	error_exit("Usage: cp file_from file_to", 97);
-
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
 	fd_f = open(argv[1], O_RDONLY);
-	if (fd_f == -1)
-	error_exit("Can't read from file", 98);
-
-	fd_t = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_t == -1)
+	if (fd_f < 0)
 {
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+}
+	fd_t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((x = read(fd_f, buffer, BUFSIZ)) > 0)
+{
+	if (fd_t < 0 || write(fd_t, buffer, x) != x)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 	close(fd_f);
-	error_exit("Can't write to file", 99);
+	exit(99);
 }
-
-	while ((br = read(fd_f, buffer, BUFF_SIZE)) > 0)
+}
+	if (x < 0)
 {
-	bw = write(fd_t, buffer, br);
-	if (bw != br)
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+}
+	a = close(fd_f);
+	b = close(fd_t);
+	if (a < 0 || b < 0)
 {
-	close(fd_f);
-	close(fd_t);
-	error_exit("Write error occurred", 99);
+	if (a < 0)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_f);
+	if (b < 0)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_t);
+	exit(100);
 }
-}
-	if (br == -1)
-{
-	close(fd_f);
-	close(fd_t);
-	error_exit("Read error occurred", 98);
-}
-	if (close(fd_f) == -1)
-	error_exit("Can't close source fd", 100);
-
-	if (close(fd_t) == -1)
-	error_exit("Can't close target fd", 100);
 	return (0);
 }
