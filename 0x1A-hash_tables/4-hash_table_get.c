a9916799774b85a7bed678aa0e08f2214df5e2be
CoolstanHackwares <stanley_mbakwe@yahoo.com>
2024-02-01 15:16:06 -0800

    >>> ht['betty'

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - A function that retrieves a value associated with a key
+ * @ht: The hash table to look in
+ * @key: The key to lookup
+ *
+ * Return: The value associated with the element, or NULL
+ * if key couldn't be found
+ * This program conforms to the betty documentation style
+ **/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *tmp;
+
+	if (!ht || !key || strlen(key) == 0)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+
+	return (NULL);
+}
